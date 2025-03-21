b771e25703f9012224e1bd50c3c26c90fe165e5c
Mehmet Nail <iskecem@hotmail.com>
2025-03-22 00:06:39 +0200

    Add project files.

---
+++ LoginDoctorPatientApp02/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using LoginDoctorPatientApp02.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginDoctorPatientApp02.Data
+{
+    public class AppDbContext : IdentityDbContext<Users>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
